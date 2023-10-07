e035d6cc7f24543d90dff57f9f3f6b0f7adb726e
AlvisWSY <alviswang2002@gmail.com>
2023-10-07 15:42:41 +0000

    Fuck Pineapple

--- 4483.py
+++ 4483.py
@@ -1,7 +1,10 @@
 import torch 
 import torchvision
 from torchvision import transforms
-import torchsummary
+from ImageFolder import ImageFolder
+import os
+
+TRAIN = False 
 
 # 图像变换
 transform = transforms.Compose([
@@ -11,7 +14,7 @@ transform = transforms.Compose([
 ])
 
 # 加载数据集
-data = torchvision.datasets.ImageFolder(root='/media/mldadmin/home/s123mdg34_04/WangShengyuan/EE4483/datasets', transform=transform)
+data = ImageFolder(root='/media/mldadmin/home/s123mdg34_04/WangShengyuan/IE4483_furryyy/datasets', transform=transform)
 loader = torch.utils.data.DataLoader(data, batch_size=32, shuffle=True)
 
 # 加载预训练模型
@@ -29,28 +32,54 @@ criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 num_epochs = 10
 
-for epoch in range(num_epochs):
-    for inputs, labels in loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-    
-    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+if TRAIN:
+    for epoch in range(num_epochs):
+        for inputs, labels in loader:
+            print("here")
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+        
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+    model.save_state_dict(f'resnet50-10epoch-loss{loss.item():.4f}.pth')
+else:
+    if os.path.exists('resnet50-10epoch-loss0.0000.pth'):
+        model.load_state_dict('resnet50-10epoch-loss0.0000.pth')
+    print("skip training")
 
 # 模型测试
 model.eval()
+model.to(device)
 correct = 0
 total = 0
 
+# construct dataframe for output
+import pandas as pd
+
+res = {
+    "img_path": [],
+    "label": [],
+    "pred": [],
+    "logits": []
+}
+
 with torch.no_grad():
-    for inputs, labels in loader:
+    for inputs, labels, paths in loader:
+        filenames = [os.path.relpath(path, os.getcwd()) for path in paths]
         inputs, labels = inputs.to(device), labels.to(device)
         outputs = model(inputs)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
+        res["img_path"].extend(filenames)
+        res["label"].extend(labels.cpu().numpy().tolist())
+        res["pred"].extend(predicted.cpu().numpy().tolist())
+        res["logits"].extend(outputs.cpu().numpy().tolist())
+
+df = pd.DataFrame(res)
+df.to_csv("result.csv", index=False)
 
 print('Accuracy: {:.2f}%'.format(correct / total * 100))
